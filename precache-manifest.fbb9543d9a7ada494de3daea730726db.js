41eb5147e924be3a4ed8cb61e199785ea29f8892
Kvinto1986 <Kvinto1986@gmail.com>
2019-03-24 09:43:18 +0300

    Updates

---
+++ precache-manifest.fbb9543d9a7ada494de3daea730726db.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/bus-trip-react/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "8caf68578f43b62f41f6",
+    "url": "/bus-trip-react/static/js/runtime~main.9f4d862d.js"
+  },
+  {
+    "revision": "048bc861825bd5dd1bb2",
+    "url": "/bus-trip-react/static/js/main.2d6946b8.chunk.js"
+  },
+  {
+    "revision": "0571b69cddc311e66ec0",
+    "url": "/bus-trip-react/static/js/2.b934461c.chunk.js"
+  },
+  {
+    "revision": "048bc861825bd5dd1bb2",
+    "url": "/bus-trip-react/static/css/main.6d1f133e.chunk.css"
+  },
+  {
+    "revision": "04eb15e98d9789d8d45e17102043e14f",
+    "url": "/bus-trip-react/index.html"
+  }
+];
